ae0173bf28b205f612201d9ad605ba49f2cac0c6
Incomplete <incomplete@aixon.co>
2021-08-06 11:19:23 +0800

    Remove some unnecessary imports

--- dge-runtime/src/helper_macro.rs
+++ dge-runtime/src/helper_macro.rs
@@ -1,18 +1,6 @@
 #[macro_export]
 macro_rules! maybe_send_to_next {
     ($msg:expr, $queue:expr, $channel:expr, $accept_failure_msg:expr, $accept_failure:path, $exchange:expr $(,)?) => {{
-        use log::debug;
-        use log::info;
-        use log::warn;
-        use serde::Serialize;
-        use serde_json;
-        use std::fmt::Display;
-
-        use $crate::rmq_primitive;
-        use $crate::rmq_primitive::Responsibility;
-        use $crate::Error;
-        use $crate::Result;
-
         // type annotation
         let queue: Option<&str> = $queue;
 
